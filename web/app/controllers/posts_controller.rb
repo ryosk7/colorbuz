cf85e58f6d6d006dcea239c79fc40a4e004b3867
ryosk7 <uchiryo7@gmail.com>
2019-05-21 00:23:10 +0900

    add posts controller

---
+++ web/app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/create'
+  get 'posts/destroy'
   get 'user/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
