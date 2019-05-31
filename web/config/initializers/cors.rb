ab8255e98123572f6cbf926d7aed933360ea2197
ryosk7 <uchiryo7@gmail.com>
2019-05-31 10:20:27 +0900

    [FIX ME] cors postができない

--- web/config/initializers/cors.rb
+++ web/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', "localhost:8000"
+    origins 'localhost:8000'
 
     resource '*',
       headers: :any,
