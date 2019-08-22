47aee3a2a387c537ee00adf1f54897307f6bec01
ryosk7 <uchiryo7@gmail.com>
2019-08-23 02:11:46 +0900

    feat(rails-api): ruby->2.6.3, rails->5.2.3対応

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -5,23 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-posts = Post.create([
-                     { title: 'Pre-fab homes', thumbnail: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', content: 'hoge'}, 
-                     { title: 'Favorite road trips', thumbnail: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', content: 'takkun'}
-                    ])
-
-  # t.string "title"
-  # t.text "thumbnail"
-  # t.text "content"
-  # t.integer "user_id"
-  # t.integer "team_id"
-  # t.datetime "created_at", null: false
-  # t.datetime "updated_at", null: false
-
-  # { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', content: 'hoge', link: '/posts/teamdetail'},
-  # { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', content: 'takkun', link: '/posts/teamdetail'},
-  # { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', content: 'ichiki', link: '/posts/teamdetail'},
-  # { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', content: 'hoge', link: '/posts/teamdetail'},
-  # { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', content: 'takkun', link: '/posts/teamdetail'},
-  # { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', content: 'ichiki', link: '/posts/teamdetail'},
